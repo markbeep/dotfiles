5e954fb8132bcdaab25881eba8d7fc2073b115b0
Mark <20665331+markbeep@users.noreply.github.com>
2026-06-03 09:21:02 +0200

    add clang/iftop

--- aconfmgr/35-course-modules.sh
+++ aconfmgr/35-course-modules.sh
@@ -3,11 +3,16 @@ AddPackage --foreign zoom # Video Conferencing and Web Conferencing Service
 AddPackage drawio-desktop # Official Electron build of draw.io
 
 # -------------- Master Thesis --------------
+AddPackage clang # C language family frontend for LLVM
+AddPackage iftop # Display bandwidth usage on an interface
+
 # Matrix client for NetSec lab communication
 AddPackage nheko # Desktop client for the Matrix protocol
+
 # Paper/journal management for MT
 AddPackage --foreign zotero-bin  # Zotero Standalone. Is a free, easy-to-use tool to help you collect, organize, cite, and share your research sources.
 AddPackage --foreign scion-tools # SCION inter-domain network architecture - CLI tools (scion, scion-pki)
 AddPackage wasmer # Universal Binaries Powered by WebAssembly
+
 # Generate dot graphics for IP prefix
 AddPackage graphviz # Graph visualization software
