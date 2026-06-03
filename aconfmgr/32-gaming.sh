37c662d898b2d7076ec19f131cc6c55a84bfc90d
Mark <20665331+markbeep@users.noreply.github.com>
2026-06-03 09:17:17 +0200

    add wine-gecko

--- aconfmgr/32-gaming.sh
+++ aconfmgr/32-gaming.sh
@@ -17,5 +17,6 @@ AddPackage --foreign opentrack # Head tracking software
 AddPackage wine       # A compatibility layer for running Windows programs
 AddPackage winetricks # Script to install various redistributable runtime libraries in Wine.
 AddPackage wine-mono  # Wine's built-in replacement for Microsoft's .NET Framework
+AddPackage wine-gecko # Wine's built-in replacement for Microsoft's Internet Explorer
 
 AddPackage --foreign littlenavmap-bin  # A Free Open Source Flight Planner, Navigation Tool, Moving Map, Airport Search, and Airport Information System for Flight Simulator X, Prepar3D, Microsoft Flight Simulator 2020, and X-Plane
